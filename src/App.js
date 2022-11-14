354ec9f523eceb3c1690836ba29da2e9b9104710
sadiegraham <109716969+sadiegraham@users.noreply.github.com>
2022-11-14 17:42:28 +1100

    Copied Code to Github

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by Teddi Russell and is{" "}
+          <a
+            href="https://github.com/teddirussell/teddi-weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://harmonious-brigadeiros-101803.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/WeatherInfo.js
@@ -0,0 +1,49 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="row">
+        <h1>{props.data.city}</h1>
+        <h2>
+          <FormattedDate date={props.data.date} />
+        </h2>
+
+        <h3 className="text-capitalize">{props.data.description}</h3>
+        <br />
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex current-weather">
+            <div className="WeatherIconMain">
+              <WeatherIcon
+                code={props.data.icon}
+                size={64}
+                className="float-left"
+              />
+            </div>
+
+            <div className="float-left">
+              <h2 className="weather">
+                <WeatherTemperature celsius={props.data.temperature} />
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <p className="unit">
+            Humidity: <span className="Humidity">{props.data.humidity}%</span>
+          </p>
+          <p className="unit">
+            Wind: <span className="Wind">{props.data.wind} km/h</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
